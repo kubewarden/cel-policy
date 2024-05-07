d82732991bb2712336308b9032fc86c709e92225
Víctor Cuadrado Juan <vcuadradojuan@suse.de>
2024-05-07 12:33:23 +0200

    test: Add TestVerifyKeylessGithubActions, cases for host failures
    Signed-off-by: Víctor Cuadrado Juan <vcuadradojuan@suse.de>

--- internal/cel/library/oci_test.go
+++ internal/cel/library/oci_test.go
@@ -43,7 +43,19 @@ func TestOCIGetManifestDigest(t *testing.T) {
 			ast, issues := env.Compile(test.expression)
 			require.Empty(t, issues)
 
-			prog, err := env.Program(ast)
+			prog, err := env.Program(ast, cel.EvalOptions(cel.OptExhaustiveEval))
+			require.NoError(t, err)
+
+			val, _, err := prog.Eval(map[string]interface{}{})
+			require.NoError(t, err)
+
+			result, err := val.ConvertToNative(reflect.TypeOf(test.expectedResult))
+			require.NoError(t, err)
+
+			require.Equal(t, test.expectedResult, result)
+		})
+	}
+}
 
 func TestVerifyPubKeysImage(t *testing.T) {
 	tests := []struct {
@@ -63,8 +75,73 @@ func TestVerifyPubKeysImage(t *testing.T) {
 			"kw.oci.verifyPubKeysImage",
 			"kw.oci.verifyPubKeysImage('myimage:latest', ['pubkey1', 'pubkey2'], {'foo': 'bar'} )",
 			map[string]interface{}{
-				"digest":  "myhash",
 				"trusted": true,
+				"digest":  "myhash",
+			},
+		},
+	}
+	for _, test := range tests {
+		t.Run(test.name, func(t *testing.T) {
+			var err error
+
+			host.Client, err = capabilities.NewSuccessfulMockWapcClient(
+				map[string]interface{}{
+					"is_trusted": true,
+					"digest":     "myhash",
+				},
+			)
+			require.NoError(t, err)
+
+			env, err := cel.NewEnv(
+				OCI(),
+			)
+			require.NoError(t, err)
+
+			ast, issues := env.Compile(test.expression)
+			require.Empty(t, issues)
+
+			prog, err := env.Program(ast, cel.EvalOptions(cel.OptExhaustiveEval))
+			require.NoError(t, err)
+
+			val, _, err := prog.Eval(map[string]interface{}{})
+			require.NoError(t, err)
+
+			result, err := val.ConvertToNative(reflect.TypeOf(test.expectedResult))
+			require.NoError(t, err)
+
+			require.Equal(t, test.expectedResult, result)
+		})
+	}
+}
+
+func TestVerifyKeylessGithubActions(t *testing.T) {
+	tests := []struct {
+		name           string
+		expression     string
+		expectedResult interface{}
+	}{
+		{
+			"kw.oci.verifyKeylessGithubActions empty annotations",
+			"kw.oci.verifyKeylessGithubActions('myimage:latest', 'octocat', 'example-repo', {})",
+			map[string]interface{}{
+				"trusted": true,
+				"digest":  "myhash",
+			},
+		},
+		{
+			"kw.oci.verifyKeylessGithubActions empty repo",
+			"kw.oci.verifyKeylessGithubActions('myimage:latest', 'octocat', '', {'foo': 'bar'})",
+			map[string]interface{}{
+				"trusted": true,
+				"digest":  "myhash",
+			},
+		},
+		{
+			"kw.oci.verifyKeylessGithubActions",
+			"kw.oci.verifyKeylessGithubActions('myimage:latest', 'octocat', 'example-repo', {'foo': 'bar'})",
+			map[string]interface{}{
+				"trusted": true,
+				"digest":  "myhash",
 			},
 		},
 	}
@@ -116,6 +193,11 @@ func TestOCIHostFailure(t *testing.T) {
 		{
 			"kw.oci.verifyPubKeysImage host failure",
 			"kw.oci.verifyPubKeysImage('myimage:latest', ['pubkey1', 'pubkey2'], {})",
+			"cannot verify image: hostcallback error",
+		},
+		{
+			"kw.oci.verifyKeylessGithubActions host failure",
+			"kw.oci.verifyKeylessGithubActions('myimage:latest', 'octocat', 'example-repo', {})",
 			"cannot verify image: hostcallback error",
 		},
 	}
