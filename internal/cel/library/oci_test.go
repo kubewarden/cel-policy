3ba49d1a7abfa1c7bd97501560c0c090cf42bf1a
Víctor Cuadrado Juan <vcuadradojuan@suse.de>
2024-05-08 13:22:59 +0200

    test: Add TestVerifyKeylessPrefixMatch, cases for host failures
    Signed-off-by: Víctor Cuadrado Juan <vcuadradojuan@suse.de>

--- internal/cel/library/oci_test.go
+++ internal/cel/library/oci_test.go
@@ -235,6 +235,63 @@ func TestVerifyKeylessExactMatch(t *testing.T) {
 		})
 	}
 }
+
+func TestVerifyKeylessPrefixMatch(t *testing.T) {
+	tests := []struct {
+		name           string
+		expression     string
+		expectedResult interface{}
+	}{
+		{
+			"kw.oci.verifyKeylessPrefixMatch empty annotations",
+			"kw.oci.verifyKeylessPrefixMatch('myimage:latest', [ verify_v2.KeylessPrefixInfo{ Issuer: 'https://github.com/login/oauth', UrlPrefix: 'https://github.com/kubewarden/app-example/.github/workflows/ci.yml@refs/tags/' } ], {})",
+			map[string]interface{}{
+				"trusted": true,
+				"digest":  "myhash",
+			},
+		},
+		{
+			"kw.oci.verifyKeylessPrefixMatch empty KeylessPrefixInfo",
+			"kw.oci.verifyKeylessPrefixMatch('myimage:latest', [], {})",
+			map[string]interface{}{
+				"trusted": true,
+				"digest":  "myhash",
+			},
+		},
+	}
+	for _, test := range tests {
+		t.Run(test.name, func(t *testing.T) {
+			var err error
+
+			host.Client, err = capabilities.NewSuccessfulMockWapcClient(
+				map[string]interface{}{
+					"is_trusted": true,
+					"digest":     "myhash",
+				},
+			)
+			require.NoError(t, err)
+
+			env, err := cel.NewEnv(
+				OCI(),
+			)
+			require.NoError(t, err)
+
+			ast, issues := env.Compile(test.expression)
+			require.Empty(t, issues)
+
+			prog, err := env.Program(ast, cel.EvalOptions(cel.OptExhaustiveEval))
+			require.NoError(t, err)
+
+			val, _, err := prog.Eval(map[string]interface{}{})
+			require.NoError(t, err)
+
+			result, err := val.ConvertToNative(reflect.TypeOf(test.expectedResult))
+			require.NoError(t, err)
+
+			require.Equal(t, test.expectedResult, result)
+		})
+	}
+}
 func TestOCIHostFailure(t *testing.T) {
 	tests := []struct {
 		name       string
@@ -259,6 +316,11 @@ func TestOCIHostFailure(t *testing.T) {
 		{
 			"kw.oci.verifyKeylessExactMatch host failure",
 			"kw.oci.verifyKeylessExactMatch('myimage:latest', [ KeylessInfo{ Issuer: 'https://github.com/login/oauth', Subject: 'mail@example.com' } ], {})",
+			"cannot verify image: hostcallback error",
+		},
+		{
+			"kw.oci.verifyKeylessPrefixMatch host failure",
+			"kw.oci.verifyKeylessPrefixMatch('myimage:latest', [ verify_v2.KeylessPrefixInfo{ Issuer: 'https://github.com/login/oauth', UrlPrefix: 'https://github.com/kubewarden/app-example/.github/workflows/ci.yml@refs/tags/' } ], {})",
 			"cannot verify image: hostcallback error",
 		},
 	}
