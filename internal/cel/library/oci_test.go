b17059b69f49994d8c777eadb7b50d6af60ae246
Víctor Cuadrado Juan <vcuadradojuan@suse.de>
2024-05-08 13:22:16 +0200

    test: Add TestVerifyKeylessExactMatch, cases for host failures
    Signed-off-by: Víctor Cuadrado Juan <vcuadradojuan@suse.de>

--- internal/cel/library/oci_test.go
+++ internal/cel/library/oci_test.go
@@ -179,6 +179,62 @@ func TestVerifyKeylessGithubActions(t *testing.T) {
 	}
 }
 
+func TestVerifyKeylessExactMatch(t *testing.T) {
+	tests := []struct {
+		name           string
+		expression     string
+		expectedResult interface{}
+	}{
+		{
+			"kw.oci.verifyKeylessExactMatch empty annotations",
+			"kw.oci.verifyKeylessExactMatch('myimage:latest', [KeylessInfo{Issuer: 'foo', Subject: 'bar' }], {})",
+			map[string]interface{}{
+				"trusted": true,
+				"digest":  "myhash",
+			},
+		},
+		{
+			"kw.oci.verifyKeylessExactMatch empty KeylessInfo",
+			"kw.oci.verifyKeylessExactMatch('myimage:latest', [], {})",
+			map[string]interface{}{
+				"trusted": true,
+				"digest":  "myhash",
+			},
+		},
+	}
+	for _, test := range tests {
+		t.Run(test.name, func(t *testing.T) {
+			var err error
+
+			host.Client, err = capabilities.NewSuccessfulMockWapcClient(
+				map[string]interface{}{
+					"is_trusted": true,
+					"digest":     "myhash",
+				},
+			)
+			require.NoError(t, err)
+
+			env, err := cel.NewEnv(
+				OCI(),
+			)
+			require.NoError(t, err)
+
+			ast, issues := env.Compile(test.expression)
+			require.Empty(t, issues)
+
+			prog, err := env.Program(ast, cel.EvalOptions(cel.OptExhaustiveEval))
+			require.NoError(t, err)
+
+			val, _, err := prog.Eval(map[string]interface{}{})
+			require.NoError(t, err)
+
+			result, err := val.ConvertToNative(reflect.TypeOf(test.expectedResult))
+			require.NoError(t, err)
+
+			require.Equal(t, test.expectedResult, result)
+		})
+	}
+}
 func TestOCIHostFailure(t *testing.T) {
 	tests := []struct {
 		name       string
@@ -198,6 +254,11 @@ func TestOCIHostFailure(t *testing.T) {
 		{
 			"kw.oci.verifyKeylessGithubActions host failure",
 			"kw.oci.verifyKeylessGithubActions('myimage:latest', 'octocat', 'example-repo', {})",
+			"cannot verify image: hostcallback error",
+		},
+		{
+			"kw.oci.verifyKeylessExactMatch host failure",
+			"kw.oci.verifyKeylessExactMatch('myimage:latest', [ KeylessInfo{ Issuer: 'https://github.com/login/oauth', Subject: 'mail@example.com' } ], {})",
 			"cannot verify image: hostcallback error",
 		},
 	}
