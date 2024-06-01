31bb295ae4d36958db350089a321775720e4fd45
Fabrizio Sestito <fabrizio.sestito@suse.com>
2024-06-01 09:23:38 +0200

    refactor: OCI CEL library
    Signed-off-by: Fabrizio Sestito <fabrizio.sestito@suse.com>

--- internal/cel/library/oci_test.go
+++ internal/cel/library/oci_test.go
@@ -1,96 +1,63 @@
-// nolint: dupl
 package library
 
 import (
-	"fmt"
+	"encoding/json"
 	"reflect"
 	"testing"
 
 	"github.com/google/cel-go/cel"
-	"github.com/kubewarden/policy-sdk-go/pkg/capabilities"
-	manifestDigest "github.com/kubewarden/policy-sdk-go/pkg/capabilities/oci/manifest_digest"
-
 	"github.com/stretchr/testify/require"
+
+	mocks "github.com/kubewarden/cel-policy/mocks/github.com/kubewarden/policy-sdk-go/pkg/capabilities"
+	manifestCap "github.com/kubewarden/policy-sdk-go/pkg/capabilities/oci/manifest"
+	manifestDigestCap "github.com/kubewarden/policy-sdk-go/pkg/capabilities/oci/manifest_digest"
+	specs "github.com/opencontainers/image-spec/specs-go/v1"
 )
 
-func TestOCIGetManifestDigest(t *testing.T) {
+func TestOCI(t *testing.T) {
 	tests := []struct {
-		name           string
-		expression     string
-		response       string
-		expectedResult string
+		name              string
+		expression        string
+		expectedOperation string
+		expectedRequest   interface{}
+		response          interface{}
+		expectedResult    interface{}
 	}{
 		{
-			"kw.oci.getManifestDigest",
-			"kw.oci.getManifestDigest('myimage:latest')",
-			"myhash",
-			"myhash",
-		},
-	}
-	for _, test := range tests {
-		t.Run(test.name, func(t *testing.T) {
-			var err error
-			host.Client, err = capabilities.NewSuccessfulMockWapcClient(manifestDigest.OciManifestResponse{
-				Digest: test.response,
-			})
-			require.NoError(t, err)
-
-			env, err := cel.NewEnv(
-				OCI(),
-			)
-			require.NoError(t, err)
-
-			ast, issues := env.Compile(test.expression)
-			require.Empty(t, issues)
-
-			prog, err := env.Program(ast, cel.EvalOptions(cel.OptExhaustiveEval))
-			require.NoError(t, err)
-
-			val, _, err := prog.Eval(map[string]interface{}{})
-			require.NoError(t, err)
-
-			result, err := val.ConvertToNative(reflect.TypeOf(test.expectedResult))
-			require.NoError(t, err)
-
-			require.Equal(t, test.expectedResult, result)
-		})
-	}
-}
-
-func TestVerifyPubKeysImage(t *testing.T) {
-	tests := []struct {
-		name           string
-		expression     string
-		expectedResult interface{}
-	}{
-		{
-			"kw.oci.verifyPubKeysImage empty annotations",
-			"kw.oci.verifyPubKeysImage('myimage:latest', ['pubkey1', 'pubkey2'], {} )",
-			map[string]interface{}{
-				"digest":  "myhash",
-				"trusted": true,
-			},
-		},
-		{
-			"kw.oci.verifyPubKeysImage",
-			"kw.oci.verifyPubKeysImage('myimage:latest', ['pubkey1', 'pubkey2'], {'foo': 'bar'} )",
-			map[string]interface{}{
-				"trusted": true,
-				"digest":  "myhash",
-			},
-		},
-	}
-	for _, test := range tests {
-		t.Run(test.name, func(t *testing.T) {
-			var err error
-
-			host.Client, err = capabilities.NewSuccessfulMockWapcClient(
-				map[string]interface{}{
-					"is_trusted": true,
-					"digest":     "myhash",
+			"manifest",
+			"kw.oci.image('image:latest').manifest().image.mediaType",
+			"v1/oci_manifest",
+			"image:latest",
+			manifestCap.OciImageManifestResponse{
+				Image: &specs.Manifest{
+					MediaType: specs.MediaTypeImageManifest,
 				},
-			)
+			},
+			specs.MediaTypeImageManifest,
+		},
+		{
+			"manifestDigest",
+			"kw.oci.image('image:latest').manifestDigest()",
+			"v1/manifest_digest",
+			"image:latest",
+			manifestDigestCap.OciManifestResponse{
+				Digest: "sha256:1234",
+			},
+			"sha256:1234",
+		},
+	}
+	for _, test := range tests {
+		t.Run(test.name, func(t *testing.T) {
+			response, err := json.Marshal(test.response)
 			require.NoError(t, err)
+
+			expectedRequest, err := json.Marshal(test.expectedRequest)
+			require.NoError(t, err)
+
+			mockWapcClient := &mocks.MockWapcClient{}
+			mockWapcClient.On("HostCall", "kubewarden", "oci", test.expectedOperation, expectedRequest).Return(response, nil)
+
+			host.Client = mockWapcClient
 
 			env, err := cel.NewEnv(
 				OCI(),
@@ -100,7 +67,7 @@ func TestVerifyPubKeysImage(t *testing.T) {
 			ast, issues := env.Compile(test.expression)
 			require.Empty(t, issues)
 
-			prog, err := env.Program(ast, cel.EvalOptions(cel.OptExhaustiveEval))
+			prog, err := env.Program(ast)
 			require.NoError(t, err)
 
 			val, _, err := prog.Eval(map[string]interface{}{})
@@ -110,302 +77,6 @@ func TestVerifyPubKeysImage(t *testing.T) {
 			require.NoError(t, err)
 
 			require.Equal(t, test.expectedResult, result)
-		})
-	}
-}
-
-func TestVerifyKeylessGithubActions(t *testing.T) {
-	tests := []struct {
-		name           string
-		expression     string
-		expectedResult interface{}
-	}{
-		{
-			"kw.oci.verifyKeylessGithubActions empty annotations",
-			"kw.oci.verifyKeylessGithubActions('myimage:latest', 'octocat', 'example-repo', {})",
-			map[string]interface{}{
-				"trusted": true,
-				"digest":  "myhash",
-			},
-		},
-		{
-			"kw.oci.verifyKeylessGithubActions empty repo",
-			"kw.oci.verifyKeylessGithubActions('myimage:latest', 'octocat', '', {'foo': 'bar'})",
-			map[string]interface{}{
-				"trusted": true,
-				"digest":  "myhash",
-			},
-		},
-		{
-			"kw.oci.verifyKeylessGithubActions",
-			"kw.oci.verifyKeylessGithubActions('myimage:latest', 'octocat', 'example-repo', {'foo': 'bar'})",
-			map[string]interface{}{
-				"trusted": true,
-				"digest":  "myhash",
-			},
-		},
-	}
-	for _, test := range tests {
-		t.Run(test.name, func(t *testing.T) {
-			var err error
-
-			host.Client, err = capabilities.NewSuccessfulMockWapcClient(
-				map[string]interface{}{
-					"is_trusted": true,
-					"digest":     "myhash",
-				},
-			)
-			require.NoError(t, err)
-
-			env, err := cel.NewEnv(
-				OCI(),
-			)
-			require.NoError(t, err)
-
-			ast, issues := env.Compile(test.expression)
-			require.Empty(t, issues)
-
-			prog, err := env.Program(ast, cel.EvalOptions(cel.OptExhaustiveEval))
-			require.NoError(t, err)
-
-			val, _, err := prog.Eval(map[string]interface{}{})
-			require.NoError(t, err)
-
-			result, err := val.ConvertToNative(reflect.TypeOf(test.expectedResult))
-			require.NoError(t, err)
-
-			require.Equal(t, test.expectedResult, result)
-		})
-	}
-}
-
-func TestVerifyKeylessExactMatch(t *testing.T) {
-	tests := []struct {
-		name           string
-		expression     string
-		expectedResult interface{}
-	}{
-		{
-			"kw.oci.verifyKeylessExactMatch empty annotations",
-			"kw.oci.verifyKeylessExactMatch('myimage:latest', [KeylessInfo{Issuer: 'foo', Subject: 'bar' }], {})",
-			map[string]interface{}{
-				"trusted": true,
-				"digest":  "myhash",
-			},
-		},
-		{
-			"kw.oci.verifyKeylessExactMatch empty KeylessInfo",
-			"kw.oci.verifyKeylessExactMatch('myimage:latest', [], {})",
-			map[string]interface{}{
-				"trusted": true,
-				"digest":  "myhash",
-			},
-		},
-	}
-	for _, test := range tests {
-		t.Run(test.name, func(t *testing.T) {
-			var err error
-
-			host.Client, err = capabilities.NewSuccessfulMockWapcClient(
-				map[string]interface{}{
-					"is_trusted": true,
-					"digest":     "myhash",
-				},
-			)
-			require.NoError(t, err)
-
-			env, err := cel.NewEnv(
-				OCI(),
-			)
-			require.NoError(t, err)
-
-			ast, issues := env.Compile(test.expression)
-			require.Empty(t, issues)
-
-			prog, err := env.Program(ast, cel.EvalOptions(cel.OptExhaustiveEval))
-			require.NoError(t, err)
-
-			val, _, err := prog.Eval(map[string]interface{}{})
-			require.NoError(t, err)
-
-			result, err := val.ConvertToNative(reflect.TypeOf(test.expectedResult))
-			require.NoError(t, err)
-
-			require.Equal(t, test.expectedResult, result)
-		})
-	}
-}
-
-func TestVerifyKeylessPrefixMatch(t *testing.T) {
-	tests := []struct {
-		name           string
-		expression     string
-		expectedResult interface{}
-	}{
-		{
-			"kw.oci.verifyKeylessPrefixMatch empty annotations",
-			"kw.oci.verifyKeylessPrefixMatch('myimage:latest', [ verify_v2.KeylessPrefixInfo{ Issuer: 'https://github.com/login/oauth', UrlPrefix: 'https://github.com/kubewarden/app-example/.github/workflows/ci.yml@refs/tags/' } ], {})",
-			map[string]interface{}{
-				"trusted": true,
-				"digest":  "myhash",
-			},
-		},
-		{
-			"kw.oci.verifyKeylessPrefixMatch empty KeylessPrefixInfo",
-			"kw.oci.verifyKeylessPrefixMatch('myimage:latest', [], {})",
-			map[string]interface{}{
-				"trusted": true,
-				"digest":  "myhash",
-			},
-		},
-	}
-	for _, test := range tests {
-		t.Run(test.name, func(t *testing.T) {
-			var err error
-
-			host.Client, err = capabilities.NewSuccessfulMockWapcClient(
-				map[string]interface{}{
-					"is_trusted": true,
-					"digest":     "myhash",
-				},
-			)
-			require.NoError(t, err)
-
-			env, err := cel.NewEnv(
-				OCI(),
-			)
-			require.NoError(t, err)
-
-			ast, issues := env.Compile(test.expression)
-			require.Empty(t, issues)
-
-			prog, err := env.Program(ast, cel.EvalOptions(cel.OptExhaustiveEval))
-			require.NoError(t, err)
-
-			val, _, err := prog.Eval(map[string]interface{}{})
-			require.NoError(t, err)
-
-			result, err := val.ConvertToNative(reflect.TypeOf(test.expectedResult))
-			require.NoError(t, err)
-
-			require.Equal(t, test.expectedResult, result)
-		})
-	}
-}
-
-func TestVerifyCertificate(t *testing.T) {
-	tests := []struct {
-		name           string
-		expression     string
-		expectedResult interface{}
-	}{
-		{
-			"kw.oci.verifyCertificate empty annotations",
-			"kw.oci.verifyCertificate('myimage:latest', 'my PEM cert', ['cert_chain1', 'cert_chain2'], false, {})",
-			map[string]interface{}{
-				"trusted": true,
-				"digest":  "myhash",
-			},
-		},
-		{
-			"kw.oci.verifyCertificate empty cert chain",
-			"kw.oci.verifyCertificate('myimage:latest', 'my PEM cert', [], false, {})",
-			map[string]interface{}{
-				"trusted": true,
-				"digest":  "myhash",
-			},
-		},
-	}
-	for _, test := range tests {
-		t.Run(test.name, func(t *testing.T) {
-			var err error
-
-			host.Client, err = capabilities.NewSuccessfulMockWapcClient(
-				map[string]interface{}{
-					"is_trusted": true,
-					"digest":     "myhash",
-				},
-			)
-			require.NoError(t, err)
-
-			env, err := cel.NewEnv(
-				OCI(),
-			)
-			require.NoError(t, err)
-
-			ast, issues := env.Compile(test.expression)
-			require.Empty(t, issues)
-
-			prog, err := env.Program(ast, cel.EvalOptions(cel.OptExhaustiveEval))
-			require.NoError(t, err)
-
-			val, _, err := prog.Eval(map[string]interface{}{})
-			require.NoError(t, err)
-
-			result, err := val.ConvertToNative(reflect.TypeOf(test.expectedResult))
-			require.NoError(t, err)
-
-			require.Equal(t, test.expectedResult, result)
-		})
-	}
-}
-
-func TestOCIHostFailure(t *testing.T) {
-	tests := []struct {
-		name       string
-		expression string
-		expected   string
-	}{
-		{
-			"kw.oci.getManifestDigest host failure",
-			"kw.oci.getManifestDigest('myimage:latest')",
-			"cannot get oci manifest: hostcallback error",
-		},
-		{
-			"kw.oci.verifyPubKeysImage host failure",
-			"kw.oci.verifyPubKeysImage('myimage:latest', ['pubkey1', 'pubkey2'], {})",
-			"cannot verify image: hostcallback error",
-		},
-		{
-			"kw.oci.verifyKeylessGithubActions host failure",
-			"kw.oci.verifyKeylessGithubActions('myimage:latest', 'octocat', 'example-repo', {})",
-			"cannot verify image: hostcallback error",
-		},
-		{
-			"kw.oci.verifyKeylessExactMatch host failure",
-			"kw.oci.verifyKeylessExactMatch('myimage:latest', [ KeylessInfo{ Issuer: 'https://github.com/login/oauth', Subject: 'mail@example.com' } ], {})",
-			"cannot verify image: hostcallback error",
-		},
-		{
-			"kw.oci.verifyKeylessPrefixMatch host failure",
-			"kw.oci.verifyKeylessPrefixMatch('myimage:latest', [ verify_v2.KeylessPrefixInfo{ Issuer: 'https://github.com/login/oauth', UrlPrefix: 'https://github.com/kubewarden/app-example/.github/workflows/ci.yml@refs/tags/' } ], {})",
-			"cannot verify image: hostcallback error",
-		},
-		{
-			"kw.oci.verifyCertificate host failure",
-			"kw.oci.verifyCertificate('myimage:latest', 'my PEM cert', ['cert_chain1', 'cert_chain2'], false, {})",
-			"cannot verify image: hostcallback error",
-		},
-	}
-	for _, test := range tests {
-		t.Run(test.name, func(t *testing.T) {
-			var err error
-			host.Client = capabilities.NewFailingMockWapcClient(fmt.Errorf("hostcallback error"))
-
-			env, err := cel.NewEnv(
-				OCI(),
-			)
-			require.NoError(t, err)
-
-			ast, issues := env.Compile(test.expression)
-			require.Empty(t, issues)
-
-			prog, err := env.Program(ast, cel.EvalOptions(cel.OptExhaustiveEval))
-			require.NoError(t, err)
-
-			_, _, err = prog.Eval(map[string]interface{}{})
-			require.Error(t, err)
-			require.Equal(t, test.expected, err.Error())
 		})
 	}
 }
